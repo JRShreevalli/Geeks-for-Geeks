266416f204647868fda0a2b32075a8793bfdba4f
J Shreevalli <70849567+JRShreevalli@users.noreply.github.com>
2022-01-09 08:54:10 +0530

    Create Count_Set_Bits.cpp

---
+++ BitMagic/Count_Set_Bits.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+  
+    int CountSetBits(int n )
+    {
+        int count = 0 ;
+        while(n > 0)
+        {
+            count += 1 ;
+            n = n & (n-1) ;
+        }
+        return count ;
+    }
+    int countBitsFlip(int a, int b)
+    {
+      return CountSetBits(a ^ b) ;
+    }
+};
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--) 
+	{
+		int a,b;
+		cin>>a>>b; 
+
+        Solution ob;
+		cout<<ob.countBitsFlip(a, b)<<endl;
+	}
+	return 0;
+}  
